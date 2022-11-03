a791d816e3d4197918156c95ae0cfe239e13c94a
Janice Yan <janyan33@gmail.com>
2022-11-02 20:20:47 -0400

    Update fig stuff for manuscript

--- scripts/longevity_analyses.R
+++ scripts/longevity_analyses.R
@@ -6,13 +6,20 @@ library(survminer)
 library(tidyverse)
 library(ggsci)
 
+My_Theme = theme(
+  axis.title.x = element_text(size = 18),
+  axis.text.x = element_text(size = 16),
+  axis.title.y = element_text(size = 18), 
+  axis.text.y = element_text(size = 16))
+
 survival <- read.csv("data/longevity.csv", stringsAsFactors = TRUE)
 
 ## Survival curves
 curve <- survfit(Surv(longevity) ~ treatment, data = survival)
 
 ggsurvplot(curve, data = survival, legend = "none", linetype = 1, size = 1.8, alpha = 0.7, color = "strata", 
-           palette = "nejm") + xlab("Day")
+           palette = "nejm", font.x = 22, 
+           font.y = 22, font.tickslab = 16) + xlab("Day") + ylab("Proportion of females alive")
 
 ## Survival model
 long_model <- coxph(Surv(longevity) ~ treatment, data = survival)
